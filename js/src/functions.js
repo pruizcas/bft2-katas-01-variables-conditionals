e057819f1db55fc6df0815712d173a94ecccb1d0
Javi <javi@jsrois.net>
2021-09-27 18:42:16 +0200

    AAlgunos tests del frontend

---
+++ js/src/functions.js
@@ -0,0 +1,9 @@
+export function isEven(number) {
+    let response = number % 2 == 0;
+    return response;
+}
+
+export function isOdd(number) {
+    let response = false;
+    return response;
+}
---
+++ js/test/test.js
@@ -0,0 +1,5 @@
+import {isEven} from "../src/functions";
+
+test("greeting", () => {
+    expect(isEven(16)).toBe(true)
+})
